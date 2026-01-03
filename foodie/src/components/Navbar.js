faf0f3bfee81c4469b2eadca01f64c7ea7569ab8
Salman Khan <starlordff2007@gmail.com>
2026-01-03 22:38:53 +0530

    Update foodie app

--- foodie/src/components/Navbar.js
+++ foodie/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Foodie</a>
+        <a className="navbar-brand" href="/">Foodie</a>
 
         <button
           className="navbar-toggler"
@@ -17,10 +17,10 @@ export default function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="navbar-nav ms-auto">
-            <a className="nav-link active" href="#">Home</a>
-            <a className="nav-link" href="#">Features</a>
-            <a className="nav-link" href="#">Pricing</a>
-            <a className="nav-link disabled" href="#" tabIndex="-1">
+            <a className="nav-link active" href="/">Home</a>
+            <a className="nav-link" href="/">Features</a>
+            <a className="nav-link" href="/">Pricing</a>
+            <a className="nav-link disabled" href="/" tabIndex="-1">
               Disabled
             </a>
           </div>
--- foodie/src/screens/Home.js
+++ foodie/src/screens/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container text-center mt-5">
       <h1>Home Screen</h1>
-      <h2 className="mt-3">Welcome to Foodie!</h2>
+      <h2 className="mt-3">Food to Foodie!</h2>
       <p className="text-muted">
         Get started by exploring our delicious recipes.
       </p>
