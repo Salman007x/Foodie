a27b04704e969f7b3c4c8960707a8ade03f85647
Salman Khan <starlordff2007@gmail.com>
2026-01-05 23:13:44 +0530

    Add Card component and integrate into Home screen

--- foodie/src/screens/Home.js
+++ foodie/src/screens/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 export default function Home() {
   return (
     <div className="container text-center mt-5">
       <Navbar />
-      <h1>Home Screen</h1>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       <h2 className="mt-3">Food to Foodie!</h2>
       <p className="text-muted">
         Get started by exploring our delicious recipes.
