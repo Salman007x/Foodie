cb2d08e95837d0e904d71b66971f73b17cb08407
Salman Khan <starlordff2007@gmail.com>
2026-01-11 23:16:14 +0530

    Fix: Wrap Route components in Routes and improve code structure

--- foodie/src/App.js
+++ foodie/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <Router>
-      <div>
+      <div className="app-container">
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
